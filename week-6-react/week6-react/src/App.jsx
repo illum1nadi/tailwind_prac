8e75b084ea297f3e14d1a00133f6224ab6662a46
illum1nadi <adityainduri@gmail.com>
2025-03-31 13:21:35 +0530

    So we used something called empty div/react fragment

--- week-6-react/week6-react/src/App.jsx
+++ week-6-react/week6-react/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Header } from './components/header'
 //cannot return two childs in a component. as it makes it easy to do reconcilation.
 
 function App() {
   return (
-    <p>Hi i am aditya</p>
-    <p>Hi i am aditya</p>
+    <>
+      <Header title = "aditya"></Header>
+      <Header title = "aditya"></Header>
+    </>
   )
 }
 
