da0e8eb609dc4805107c6451106623eb8a250066
illum1nadi <adityainduri@gmail.com>
2025-03-31 18:31:59 +0530

    This is the basic todo application required to test keys.

--- week-6-react/key-todo/src/App.jsx
+++ week-6-react/key-todo/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Todo } from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    id : 1,
+    title : "gym jao bc.",
+    description : "jaa raha hun bc."
+  }, 
+  {
+    id : 2,
+    title : "gym jao bc.",
+    description : "jaa raha hun bc."
+  }, 
+  {
+    id : 3,
+    title : "gym jao bc.",
+    description : "jaa raha hun bc."
+  }])
 
+  function addTodo() {
+    setTodos([...todos, {
+      id : 4, 
+      title : "gym jao bc.",
+      description : "jaa raha hun bc."
+    }])
+  }
+  
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" className='title' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <input type="text" className='description' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <button onClick={addTodo}>Add a Todo</button>
+      </div>
+      {todos.map(element => 
+        <Todo title = {element.title} description = {element.description}></Todo>
+      )}
+    </div>
   )
 }
 
