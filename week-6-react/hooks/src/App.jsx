f88d7005ef703a6ad0779b9039a58227ce1b06de
illum1nadi <adityainduri@gmail.com>
2025-03-31 20:24:29 +0530

    Used the use effect hook and fetched some todos from a backend server.

---
+++ week-6-react/hooks/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import { Todos } from './Todos'
+
+function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3002/todos")
+    .then(async (res) => {
+      const json = await res.json();
+      setTodos(json);
+    })
+  })
+
+  return (
+    <>
+      <Todos todos = {todos}></Todos>
+    </>
+  )
+}
+
+export default App
