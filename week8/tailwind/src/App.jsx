1d556d63e6320dec70a672fe8e98fc39e9c5d6fa
illum1nadi <adityainduri@gmail.com>
2025-04-04 18:32:54 +0530

    Making a website reactive using tailwind.

--- week8/tailwind/src/App.jsx
+++ week8/tailwind/src/App.jsx
@@ -6,10 +6,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='flex justify-between'>
-        <div className='bg-blue-500'>HI</div>
-        <div className='bg-cyan-500'>Hello</div>
-        <div className='bg-pink-500'>How are you</div>
+      <div className='grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2'>
+        <div className='bg-blue-500 col-span-1'>HI</div>
+        <div className='bg-cyan-500 col-span-1'>Hello</div>
+        <div className='bg-pink-500 col-span-1'>How are you</div>
       </div>
     </>
   )
