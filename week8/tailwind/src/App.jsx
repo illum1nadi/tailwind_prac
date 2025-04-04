9a9ef9d69ba76d2b2fd4365e48d10268d412bfbd
illum1nadi <adityaroxx127@gmail.com>
2025-04-04 19:09:39 +0530

    Made a responsive page using tailwind

--- week8/tailwind/src/App.jsx
+++ week8/tailwind/src/App.jsx
@@ -7,11 +7,12 @@ function App() {
   return (
     <>
 
-    
+
       <div className='grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2'>
         <div className='bg-blue-500 col-span-1'>HI</div>
         <div className='bg-cyan-500 col-span-1'>Hello</div>
         <div className='bg-pink-500 col-span-1'>How are you</div>
+        
       </div>
     </>
   )
